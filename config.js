a28dae3014bf7ce654e6daabf7ca3f339bdc88fc
fwaehz <zizouana2@gmail.com>
2025-09-27 20:50:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '.', // Préfixe - Le texte avant les commandes
         buyer: '924837907959795814', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: 'Zprotect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: '/nyuk', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
